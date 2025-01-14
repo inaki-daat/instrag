c6e98f07f4f78406e0799cb69636ee857aa7790f
Iñaki <inaki@pa-dhcp-10-120-229-50.gp-vpn.northwestern.private>
2025-01-14 16:58:41 -0600

    added cors, changed docker

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ logger.info("Starting FastAPI application")
 app = FastAPI(title=settings.app_name)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your frontend URL
+    allow_origins=["*"],  # Your frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
